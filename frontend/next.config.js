7517371f50f2844dade6e1b1a7670f72559a9d4a
ANS <36783010+answebdev@users.noreply.github.com>
2023-05-23 16:06:40 -0700

    add schemas

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  images: {
+    domains: ['cdn.sanity.io'],
+  },
+};
--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -17,7 +17,7 @@ export default function Home() {
                 </h1>
             </main>
 
-            {/* <footer className={styles.footer}>
+            <footer className={styles.footer}>
                 <a
                     href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                     target="_blank"
@@ -28,7 +28,7 @@ export default function Home() {
                         <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
                     </span>
                 </a>
-            </footer> */}
+            </footer>
         </div>
     );
 }
