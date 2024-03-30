4b5a3a0cab6c2458f6c73dfe2e4dbd4317b3c248
ANS <36783010+answebdev@users.noreply.github.com>
2024-03-30 12:24:09 -0700

    fix console error

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,33 @@
 import Layout from '../components/Layout';
+import { Source_Sans_Pro, Source_Serif_Pro } from 'next/font/google';
 import '../styles/globals.css';
 import '../styles/prism-material-dark.css';
+
+const source_sans_pro = Source_Sans_Pro({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-sans',
+});
+
+const source_serif_pro = Source_Serif_Pro({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-serif',
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <style jsx global>{`
+      :root {
+        --source_sans_pro-font: ${source_sans_pro.style.fontFamily};
+        --source_serif_pro-font: ${source_serif_pro.style.fontFamily};
+      }
+    `}</style>
       <Component {...pageProps} />
     </Layout>
   );
